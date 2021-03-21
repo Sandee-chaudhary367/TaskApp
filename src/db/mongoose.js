d6ab196d68d10d39e6649a82f86a7592bd6193c8
Sandeep Chaudhary <sandee.chaudhary367@gmail.com>
2021-03-21 12:30:21 +0530

    Writing the description 2 by Sandeep

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 //setup to connect to the database
+//mongoose.connect('process.env.MONGO_URI', {
 mongoose.connect('mongodb://127.0.0.1:27017/taskapp', {
     useNewUrlParser: true,
     useCreateIndex: true,
